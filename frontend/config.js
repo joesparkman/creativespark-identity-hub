b16fc330d4e184773b37c1b9092c5da2ebfc6498
joesparkman <joe@joesparkman.com>
2026-06-27 01:48:05 -0400

    Fix config.js APP_CONFIG and SESSION_TOKEN

--- frontend/config.js
+++ frontend/config.js
@@ -1,1 +1,3 @@
-const API_URL = "https://bito4xs7s6.execute-api.us-east-2.amazonaws.com/Prod/upload";
+window.APP_CONFIG = {
+  API_URL: "https://bito4xs7s6.execute-api.us-east-2.amazonaws.com/Prod"
+};
